6d7e48594c0c38cfbaee524de45e15939feab22b
ricodejong02 <ricodejong02@gmail.com>
2016-10-03 17:48:40 +0200

    Added chickens

--- TextBasedGame/TextBasedGame/RPG/RGame/Mob/Chicken.cs
+++ TextBasedGame/TextBasedGame/RPG/RGame/Mob/Chicken.cs
@@ -3,9 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPG.Utilities;
 
 namespace RPG.RGame.Mob {
-    public class Chicken : Mob{
+    internal class Chicken : Mob {
+        public Chicken() {
+            Damage = 0;
+            totalHealth = 5;
+            remainingHealth = 5;
+            Name = "Chicken";
+        }
 
+        public int Damage { get; }
+        public int totalHealth { get; }
+        public int remainingHealth { get; set; }
+        public int Defense { get; set; }
+        public string Name { get; set; }
+
+        public void Attack(Player.Player _player, Mob _chicken) {
+            _player.Health -= Damage;
+        }
+
+        public void recieveAttack(Player.Player _player, Mob _chicken) {
+            remainingHealth -= _player.holdingWeapon.Damage;
+            if (remainingHealth <= 0) {
+                Message.SendLine("Choock, choock!", ConsoleColor.Red);               
+            }
+        }
     }
 }
--- TextBasedGame/TextBasedGame/RPG/Utilities/Message.cs
+++ TextBasedGame/TextBasedGame/RPG/Utilities/Message.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Media;
 using System.Threading;
 
-namespace TBC
+namespace RPG.Utilities
 {
 
     /// <summary>
