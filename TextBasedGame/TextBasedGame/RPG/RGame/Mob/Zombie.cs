a6a16ef5d10f3e988ea2ef55cb6d080ac0c24355
ricodejong02 <ricodejong02@gmail.com>
2016-10-03 17:43:07 +0200

    R1
    bug fixed in zombie.cs

--- TextBasedGame/TextBasedGame/RPG/RGame/Mob/Zombie.cs
+++ TextBasedGame/TextBasedGame/RPG/RGame/Mob/Zombie.cs
@@ -19,11 +19,11 @@ namespace RPG.RGame.Mob {
         public int Defense { get; set; }
         public string Name { get; set; }
 
-        public void Attack(Player.Player _player, Zombie _zombie){
+        public void Attack(Player.Player _player, Mob _zombie){
             _player.Health -= Damage;
         }
 
-        public void recieveAttack(Player.Player _player, Zombie _zombie)
+        public void recieveAttack(Player.Player _player, Mob _zombie)
         {
             remainingHealth -= _player.holdingWeapon.Damage;
         }
